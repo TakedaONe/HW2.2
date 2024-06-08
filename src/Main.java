cd4e7218fa780b647ec1f8c7f908ec3e90aded11
TakedaONe <146793652+TakedaONe@users.noreply.github.com>
2024-06-08 16:12:52 +0600

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
 public class Main {
+    public static Parent createObject(String className) {
+        switch (className) {
+            case "Child1":
+                return new ChildOne("Common Property for Child1", 10);
+            case "Child2":
+                return new ChildTwo("Common Property for Child2", 20.5);
+            case "Child3":
+                return new ChildThree("Common Property for Child3", "Unique Property 3");
+            default:
+                return null;
+        }
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Parent obj1 = createObject("Child1");
+        Parent obj2 = createObject("Child2");
+        Parent obj3 = createObject("Child3");
+
+        Printable[] objects = {(Printable) obj1, (Printable) obj2, (Printable) obj3};
+
+        for (Printable obj : objects) {
+            obj.print();
+        }
     }
 }
